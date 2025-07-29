d117668543d2b8e2773ea7d1f84e83d4b5e09068
Tomoki108 <shinonome81@gmail.com>
2025-07-29 18:02:52 +0900

    mod

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
